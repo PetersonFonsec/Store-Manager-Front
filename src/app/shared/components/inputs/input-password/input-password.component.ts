7546c255c2e5523f97d6016cf4087fe06a6cfa72
Peterson <petersonsimiao1@gmail.com>
2021-12-07 21:46:38 -0300

    feat(password component): Montado componente password

--- src/app/shared/components/inputs/input-password/input-password.component.ts
+++ src/app/shared/components/inputs/input-password/input-password.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-input-password',
   templateUrl: './input-password.component.html',
-  styleUrls: ['./input-password.component.scss']
+  styleUrls: ['./input-password.component.scss'],
+  providers: [
+    {
+      multi: true,
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputPasswordComponent),
+    },
+  ],
 })
-export class InputPasswordComponent implements OnInit {
+export class InputPasswordComponent implements OnInit, ControlValueAccessor {
+  @Input() placeholder = '';
+  @Input() label = '';
+  @Input() id = '';
+  type = 'text';
+  icon = faEye;
+  val = '';
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  changeType(): void {
+    if (this.type === 'text') {
+      this.type = 'password';
+      this.icon = faEye;
+    } else {
+      this.type = 'text';
+      this.icon = faEyeSlash;
+    }
   }
 
+  onChange: any = () => {};
+
+  onTouched: any = () => {};
+
+  get value() {
+    return this.val;
+  }
+
+  set value(val) {
+    if (!val) return;
+
+    this.val = val;
+    this.onChange(val);
+    this.onTouched(val);
+  }
+
+  onBlur() {}
+
+  writeValue(value: any) {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouched = fn;
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AsideComponent } from './components/aside/aside.component';
 import { ButtonComponent } from './components/button/button.component';
 import { InputTextComponent } from './components/inputs/input-text/input-text.component';
 import { InputPasswordComponent } from './components/inputs/input-password/input-password.component';
 
 @NgModule({
-  declarations: [AsideComponent, ButtonComponent, InputTextComponent, InputPasswordComponent],
-  imports: [CommonModule],
-  exports: [AsideComponent, ButtonComponent, InputTextComponent],
+  declarations: [
+    AsideComponent,
+    ButtonComponent,
+    InputTextComponent,
+    InputPasswordComponent,
+  ],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [
+    AsideComponent,
+    ButtonComponent,
+    InputTextComponent,
+    InputPasswordComponent,
+  ],
 })
 export class SharedModule {}
