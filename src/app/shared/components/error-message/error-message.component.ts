7e859ccd41f35e8a5599c589f7482ba58e5bb330
Peterson <petersonsimiao1@gmail.com>
2022-01-29 20:47:52 -0300

    feat(error-message): criado componente que printa a mensagem de erro dos inputs

---
+++ src/app/shared/components/error-message/error-message.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { NgModel } from '@angular/forms';
+
+@Component({
+  selector: 'app-error-message',
+  templateUrl: './error-message.component.html',
+  styleUrls: ['./error-message.component.scss'],
+})
+export class ErrorMessageComponent {
+  @Input() model!: NgModel;
+}
