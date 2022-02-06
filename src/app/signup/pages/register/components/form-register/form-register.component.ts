b215162e8be709bec0bbce80248890ce77fcf528
Peterson <petersonsimiao1@gmail.com>
2022-02-06 17:46:20 -0300

    feat(FormRegister Component): Intergrado formulário de cadastro

--- src/app/signup/pages/register/components/form-register/form-register.component.ts
+++ src/app/signup/pages/register/components/form-register/form-register.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-form-register',
   templateUrl: './form-register.component.html',
   styleUrls: ['./form-register.component.scss'],
 })
-export class FormRegisterComponent implements OnInit {
+export class FormRegisterComponent implements OnDestroy {
+  subscription!: Subscription;
+  errorMessage = '';
   loading = false;
   user: any = {};
 
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 
-  submit(): void {}
+  submit(): void {
+    this.loading = true;
+
+    this.subscription = this.authService.signup(this.user).subscribe({
+      next: async () => await this.router.navigateByUrl('/'),
+      error: ({ message }) => (this.errorMessage = message),
+      complete: () => (this.loading = false),
+    });
+  }
 }
