650f1e9862e73748144f6d86bf5a195c803fe510
Peterson <petersonsimiao1@gmail.com>
2022-02-06 17:57:41 -0300

    fix(Logged Guard): Removendo parametros não ultilizado

--- src/app/shared/guards/logged.guard.ts
+++ src/app/shared/guards/logged.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
 
 @Injectable({
@@ -8,7 +8,7 @@ import { AuthService } from '../services/auth/auth.service';
 export class LoggedGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  async canActivate(route: ActivatedRouteSnapshot): Promise<boolean> {
+  async canActivate(): Promise<boolean> {
     const token = this.authService.token;
     if (!token) await this.router.navigateByUrl('/signup');
     return true;
