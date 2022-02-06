fdb8d05fc54f7bbe6777d6172e3c17d4b161e9b0
Peterson <petersonsimiao1@gmail.com>
2022-02-06 18:07:57 -0300

    feat(FormLogin Component): Integrado formulário de login

--- src/app/signup/pages/login/components/form-login/form-login.component.ts
+++ src/app/signup/pages/login/components/form-login/form-login.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-form-login',
   templateUrl: './form-login.component.html',
   styleUrls: ['./form-login.component.scss'],
 })
-export class FormLoginComponent implements OnInit {
-  user: any = {};
+export class FormLoginComponent implements OnDestroy {
+  subscription!: Subscription;
+  errorMessage = '';
   loading = false;
-  constructor() {}
+  user: any = {};
 
-  ngOnInit(): void {}
-  submit(): void {}
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  submit(): void {
+    this.loading = true;
+
+    this.subscription = this.authService.login(this.user).subscribe({
+      next: async () => await this.router.navigateByUrl('/'),
+      error: () => (this.loading = false),
+      complete: () => (this.loading = false),
+    });
+  }
 }
