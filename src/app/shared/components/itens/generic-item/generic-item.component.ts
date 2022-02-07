b30383ea57f2cc80dbb8c6ae57b49da00c750855
Peterson <petersonsimiao1@gmail.com>
2022-02-06 21:30:12 -0300

    feat(generic item componet): Criado component

---
+++ src/app/shared/components/itens/generic-item/generic-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-item',
+  templateUrl: './generic-item.component.html',
+  styleUrls: ['./generic-item.component.scss'],
+})
+export class GenericItemComponent {}
