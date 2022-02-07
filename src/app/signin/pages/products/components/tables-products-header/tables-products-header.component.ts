dd3295514b7de022bc849a6ae2d55e2ac392df27
Peterson <petersonsimiao1@gmail.com>
2022-02-06 21:31:55 -0300

    fix(product module): Movido componente table-product

--- src/app/signin/pages/products/components/tables-products-header/tables-products-header.component.ts
+++ src/app/signin/pages/products/components/tables-products-header/tables-products-header.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tables-products-header',
   templateUrl: './tables-products-header.component.html',
   styleUrls: ['./tables-products-header.component.scss'],
 })
-export class TablesProductsHeaderComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class TablesProductsHeaderComponent {}
--- src/app/signin/pages/products/components/tables-products-item/tables-products-item.component.ts
+++ src/app/signin/pages/products/components/tables-products-item/tables-products-item.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import { IProduct } from 'src/app/signin/pages/products/interfaces/products';
 
 @Component({
   selector: 'app-tables-products-item',
   templateUrl: './tables-products-item.component.html',
   styleUrls: ['./tables-products-item.component.scss'],
 })
-export class TablesProductsItemComponent implements OnInit {
+export class TablesProductsItemComponent {
+  @Input() product!: IProduct;
   optionsIcon = faEllipsisV;
-
-  ngOnInit(): void {}
 }
