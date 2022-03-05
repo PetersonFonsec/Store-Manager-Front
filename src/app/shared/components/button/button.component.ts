29ee4c3b33c51c2a400a601a221dda90c3290f42
Peterson <petersonsimiao1@gmail.com>
2022-03-05 12:28:30 -0300

    feat(button): Adicionado parametro de cor

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -8,4 +8,5 @@ import { Component, Input } from '@angular/core';
 export class ButtonComponent {
   @Input() disabled = false;
   @Input() loading = false;
+  @Input() color: 'blue' | 'red' = 'blue';
 }
