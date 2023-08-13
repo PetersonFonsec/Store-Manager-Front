a0648a0d898cfa381ba0d4c16e36b529031e6289
peterson <petersonsimiao1@gmail.com>
2023-08-12 23:43:36 -0300

    feat: adicionado mascara nas tabelas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID, DEFAULT_CURRENCY_CODE} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { StoreModule } from '@ngrx/store';
 
 import { ErrorInterceptor } from './shared/interceptors/error/error.interceptor';
 import { LoggedGuard } from './shared/guards/logged/logged.guard';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
-import { userReducer, userReducers } from './shared/stores/reducers/user.reducers';
+import { userReducer } from './shared/stores/reducers/user.reducers';
+
+registerLocaleData(localePt);
 
 export const ERROR_INTERCEPTOR_PROVIDER = {
   provide: HTTP_INTERCEPTORS,
@@ -30,7 +34,12 @@ export const ERROR_INTERCEPTOR_PROVIDER = {
     StoreModule.forRoot({ user: userReducer}),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
-  providers: [LoggedGuard, ERROR_INTERCEPTOR_PROVIDER],
+  providers: [LoggedGuard, ERROR_INTERCEPTOR_PROVIDER,
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    },
+],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
