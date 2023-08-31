422a7d20b3c886ba98550c7320d6f24618a995e0
peterson <petersonsimiao1@gmail.com>
2023-08-31 18:04:02 -0300

    fix: modulo dashboard

--- src/app/signin/pages/dashboard/dashboard.module.ts
+++ src/app/signin/pages/dashboard/dashboard.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { DashboardService } from './services/dashboard/dashboard.service';
 
 @NgModule({
   declarations: [DashboardComponent],
+  providers: [DashboardService],
   imports: [CommonModule, DashboardRoutingModule, SharedModule],
   exports: [DashboardComponent],
 })
--- src/app/signin/pages/dashboard/services/dashboard/dashboard.service.ts
+++ src/app/signin/pages/dashboard/services/dashboard/dashboard.service.ts
@@ -2,9 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BaseService } from 'src/app/shared/services/base/base.service';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class DashboardService extends BaseService {
   constructor(private http: HttpClient) {
     super('dashboard', http);
